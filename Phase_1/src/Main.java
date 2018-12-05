ee2bee82c0e80df55bac9ef638353e9fc48f7ad6
Astro333 <ali.gplayer@gmail.com>
2018-12-05 20:49:41 +0330

    Add .idea files to gitignore

--- Phase_1/src/Main.java
+++ Phase_1/src/Main.java
@@ -3,7 +3,7 @@ import Controllers.MainMenuController;
 public class Main {
     private static final String pathToPlayersInfoDirectory = "PlayersData";
     public static void main(String[] args) {
-        MainMenuController mainMenuController = new MainMenuController(pathToPlayersInfoDirectory);
+        MainMenuController mainMenuController = new MainMenuController();
         mainMenuController.startProcessing();
     }
 }
